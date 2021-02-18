341bd90feccf4b114a452320fd89e5ec7341af91
Steven Moyano <stevenmoyano1@gmail.com>
2021-02-18 14:24:15 -0600

    completed functions

---
+++ main.js
@@ -0,0 +1,31 @@
+function thinkingHello(str) {
+    return str + "???";
+}
+console.log(thinkingHello("hello"));
+
+function spaceBetweenWords(str1, str2) {
+    return str1 + " " + str2;
+}
+console.log(spaceBetweenWords("hello","world"));
+
+function difference(num1, num2, num3) {
+    return num1 - num2 - num3;
+}
+console.log(difference(10, 1, 2));
+
+function reverseDifference(num1,num2,num3) {
+    return num3 - num2 - num1;
+}
+console.log(reverseDifference(10, 1, 2));
+
+function triplestr(str) {
+    return str + str + str;
+}
+console.log(triplestr("hello"));
+
+function square(num) {
+    return Math.sqrt(num);
+}
+console.log(square(16));
+
+
